f4ea36625b1f58d363b534d69ab676a8abcf2ce5
Leigh-ann <leighann.gant@gmail.com>
2018-03-02 11:57:31 +0000

    End game feature test added for full grid

--- spec/feature_tests/end_of_game_spec.rb
+++ spec/feature_tests/end_of_game_spec.rb
@@ -12,5 +12,8 @@ describe 'game over' do
   it 'ends game when all spaces in grid are full' do
     fill_game
     expect { game.add_move(2, 2) }.to raise_error 'Game over'
+    expect(game.grid.show).to eq [['X', 'O', 'X'],
+                                  ['O', 'X', 'O'],
+                                  ['X', 'O', 'X']]
   end
 end
