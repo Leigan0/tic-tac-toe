a6ee506f6bac6e2fc8bcd087d2f6685f8b93501f
Leigh-ann <leighann.gant@gmail.com>
2018-03-02 12:07:29 +0000

    move amended to co-ordinates within grid class, I have renamed this to increase readability of the class

--- lib/grid.rb
+++ lib/grid.rb
@@ -7,10 +7,10 @@ class Grid
     grid.dup
   end
 
-  def update_field(move)
-    @move = move
-    validate_move
-    @grid[move[:y]][move[:x]] = move[:turn]
+  def update_field(coordinates)
+    @coordinates = coordinates
+    validate_field
+    @grid[coordinates[:y]][coordinates[:x]] = coordinates[:turn]
   end
 
   def full?
@@ -18,20 +18,20 @@ class Grid
   end
 
   private
-  attr_reader :move
+  attr_reader :coordinates
   attr_reader :grid
 
-  def validate_move
+  def validate_field
     fail 'Outside Grid' unless within_grid
     fail 'Already taken' unless field_empty
   end
 
   def within_grid
-    move[:y] < grid.length && move[:x] < grid[0].length
+    coordinates[:y] < grid.length && coordinates[:x] < grid[0].length
   end
 
   def field_empty
-    grid[move[:y]][move[:x]].nil?
+    grid[coordinates[:y]][coordinates[:x]].nil?
   end
 
 end
